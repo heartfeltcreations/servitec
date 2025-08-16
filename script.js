833361bd77d637ee0c711d2c7bac1a2e8b9f38d4
heartfeltcreations <heartfeltcreations.contact@gmail.com>
2025-08-16 00:19:44 -0300

    modified:   index.html 	new file:   script.js 	modified:   style.css

---
+++ script.js
@@ -0,0 +1,15 @@
+ document.querySelectorAll('.dropbtn').forEach(btn => {
+    btn.addEventListener('click', function () {
+      let menu = this.nextElementSibling;
+      menu.classList.toggle("show");
+    });
+  });
+
+  // Cerrar si hago click fuera
+  window.addEventListener('click', function(e) {
+    if (!e.target.matches('.dropbtn')) {
+      document.querySelectorAll(".dropdown-content").forEach(menu => {
+        menu.classList.remove("show");
+      });
+    }
+  });
